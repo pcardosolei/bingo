02fbfe765360af8f765817417aec95a20872d189
Paulo <paulo@bindtuning.com>
2019-11-29 01:54:39 +0000

    Adding pretty much all the logic to the bingo service.
    Adding player component for some tests.
    
    Now i need to design the css and create the add more players option and have that option done with the card logic

--- src/app/bingo.service.ts
+++ src/app/bingo.service.ts
@@ -1,28 +1,48 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, ReplaySubject } from 'rxjs';
+import { BehaviorSubject, ReplaySubject, Subject, timer } from 'rxjs';
+import { map, takeUntil} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BingoService {
 
-  lastNumber$ = new BehaviorSubject(-1);
-  bingoNumbers$ = new ReplaySubject(5);
+  // Info for the "players"
+  protected lastNumber$ = new BehaviorSubject(-1);
+  protected bingoNumbers$ = new ReplaySubject(5);
 
   // the numbers that will be drawn
   bingoStructure: Array<number>;
 
+  // flag when someone has a bingo
+  unsubscribe$ = new Subject();
+
   constructor() { }
 
-  startBingo() {}
+  startBingo() {
+    const bingoDraw = timer(0, 2000).pipe( map( () => this.drawNumber()) , takeUntil(this.unsubscribe$)).subscribe(
+      (newNumber: number) => {
+        this.populateSubjects(newNumber);
+      }
+    );
+  }
 
   // reset the bingo
   resetBingo() {
     this.bingoStructure = [...Array(75).keys()];
+    this.unsubscribe$ = new Subject();
   }
 
   // draw a number
   drawNumber() {
+    // no more balls left to draw
+    if (this.bingoStructure.length === 0) {
+      this.unsubscribe$.next();
+      this.unsubscribe$.complete();
+      return;
+    }
+
+    // draw one ball and show it to the players.
     const drawIndex = Math.random() * this.bingoStructure.length;
     const drawnNumber = this.bingoStructure.splice(drawIndex, 1);
     return drawnNumber[0];
@@ -32,5 +52,20 @@ export class BingoService {
   populateSubjects(drawnValue: number) {
     this.bingoNumbers$.next(drawnValue);
     this.lastNumber$.next(drawnValue);
+  }
+
+  // someone made BINGO!!!!
+  bingo() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  /* Observers */
+  lastNumberAsObservable() {
+    return this.lastNumber$.asObservable();
+  }
+
+  bingoNumbersAsObservable() {
+    return this.bingoNumbers$.asObservable();
   }
 }
